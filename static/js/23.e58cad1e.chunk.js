18c3529086a3c01f7f229dc4f2eab76847125261
BurritoFreak <chris.sarmousakis@gmail.com>
2023-01-20 15:41:15 -0500

    Updates

---
+++ static/js/23.e58cad1e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkittens_website=self.webpackChunkkittens_website||[]).push([[23],{2023:function(e,l,t){t.r(l);var r=t(2791);l.default=function(e){var l=e.id;return(0,e.renderWrap)(r.createElement("iframe",{height:"700px",width:"100%",frameBorder:"no",allowFullScreen:!0,src:"https://repl.it/".concat(l,"?lite=true"),scrolling:"no",sandbox:"allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"}))}}}]);
+//# sourceMappingURL=23.e58cad1e.chunk.js.map
