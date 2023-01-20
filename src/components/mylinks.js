27c44c4f10a7ff3b308dc86b3a0d740d7aa7cbc6
BurritoFreak <chris.sarmousakis@gmail.com>
2023-01-20 00:09:12 -0500

    main

--- src/components/mylinks.js
+++ src/components/mylinks.js
@@ -1,9 +1,26 @@
 
-const myLinks = () => {
-    return ([{
-        "name": "insta",
-        "url": "https://www.instagram.com/emokitten_xoxo/"
-    }])
-};
+//React element in json form with all of my links
 
-export default myLinks;
+const myLinks = [{
+        "name": "insta",
+        "url": "https://www.instagram.com/emokitten_xoxo/",
+        "icon": "fab fa-instagram"
+    },{
+        "name": "insta",
+        "url": "https://www.instagram.com/emokitten_xoxo/",
+        "icon": "fab fa-instagram"
+    },{
+        "name": "insta",
+        "url": "https://www.instagram.com/emokitten_xoxo/",
+        "icon": "fab fa-instagram"
+    },{
+        "name": "insta",
+        "url": "https://www.instagram.com/emokitten_xoxo/",
+        "icon": "fab fa-instagram"
+    },{
+        "name": "insta",
+        "url": "https://www.instagram.com/emokitten_xoxo/",
+        "icon": "fab fa-instagram"
+    }];
+
+export default myLinks
--- src/pages/links.js
+++ src/pages/links.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import myLinks from '../components/mylinks';
+
+const templinks = [{
+  "name": "insta",
+  "url": "https://www.instagram.com/emokitten_xoxo/"
+},{
+  "name": "insta",
+  "url": "https://www.instagram.com/emokitten_xoxo/"
+},{
+  "name": "insta",
+  "url": "https://www.instagram.com/emokitten_xoxo/"
+},{
+  "name": "insta",
+  "url": "https://www.instagram.com/emokitten_xoxo/"
+},{
+  "name": "insta",
+  "url": "https://www.instagram.com/emokitten_xoxo/"
+}]
+
+const results = [];
+
+  templinks.forEach((linked, index) => {
+    results.push(
+      <div key={index}>
+        <a href={linked.url} class="links">{linked.name}</a>
+      </div>,
+    );
+  });
   
-const Links = () => {
-  return (
-    <h1>You can write your blogs!</h1>
-  );
-};
-  
+  const Links = () => {
+    return <div>{results}</div>;
+  };
+
 export default Links;
