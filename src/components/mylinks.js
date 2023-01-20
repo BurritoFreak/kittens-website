bb84fcf66396cb242119cdb8c5edbc0101b4349a
BurritoFreak <chris.sarmousakis@gmail.com>
2023-01-20 00:16:14 -0500

    Fixed Links

--- src/components/mylinks.js
+++ src/components/mylinks.js
@@ -2,25 +2,37 @@
 //React element in json form with all of my links
 
 const myLinks = [{
-        "name": "insta",
+        "name": "Insta",
         "url": "https://www.instagram.com/emokitten_xoxo/",
         "icon": "fab fa-instagram"
     },{
-        "name": "insta",
-        "url": "https://www.instagram.com/emokitten_xoxo/",
-        "icon": "fab fa-instagram"
+        "name": "OnlyFans",
+        "url": "https://onlyfans.com/emokitten_xoxo",
+        "icon": "fab fa-onlyfans"
     },{
-        "name": "insta",
-        "url": "https://www.instagram.com/emokitten_xoxo/",
-        "icon": "fab fa-instagram"
+        "name": "Tik Tok 1",
+        "url": "https://www.tiktok.com/@emokitten_xoxo",
+        "icon": "fab fa-tiktok"
     },{
-        "name": "insta",
-        "url": "https://www.instagram.com/emokitten_xoxo/",
-        "icon": "fab fa-instagram"
+        "name": "Tik Tok 2",
+        "url": "https://www.tiktok.com/@emokitten_xoxo_",
+        "icon": "fab fa-tiktok"
     },{
-        "name": "insta",
-        "url": "https://www.instagram.com/emokitten_xoxo/",
-        "icon": "fab fa-instagram"
+        "name": "Tik Tok 3",
+        "url": "https://www.tiktok.com/@emokittenxoxox",
+        "icon": "fab fa-tiktok"
+    },{
+        "name": "Reddit",
+        "url": "https://www.reddit.com/user/emokitten_xoxo/",
+        "icon": "fab fa-reddit"
+    },{
+        "name": "Twitter",
+        "url": "https://twitter.com/emokitten_xoxo",
+        "icon": "fab fa-twitter"
+    },{
+        "name": "Twitch",
+        "url": "https://www.twitch.tv/emokitten_xoxo",
+        "icon": "fab fa-twitch"
     }];
 
 export default myLinks
--- src/pages/links.js
+++ src/pages/links.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import myLinks from '../components/mylinks';
 
-const templinks = [{
-  "name": "insta",
-  "url": "https://www.instagram.com/emokitten_xoxo/"
-},{
-  "name": "insta",
-  "url": "https://www.instagram.com/emokitten_xoxo/"
-},{
-  "name": "insta",
-  "url": "https://www.instagram.com/emokitten_xoxo/"
-},{
-  "name": "insta",
-  "url": "https://www.instagram.com/emokitten_xoxo/"
-},{
-  "name": "insta",
-  "url": "https://www.instagram.com/emokitten_xoxo/"
-}]
-
 const results = [];
 
-  templinks.forEach((linked, index) => {
+  myLinks.forEach((linked, index) => {
     results.push(
       <div key={index}>
         <a href={linked.url} class="links">{linked.name}</a>
