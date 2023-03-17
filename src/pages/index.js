7c3e7d8888fd747b1c7be4d7781ce57988405c3d
BurritoFreak <chris.sarmousakis@gmail.com>
2023-03-16 22:15:24 -0400

    removed neon :(

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ return (
 	<div>
 		<h1>Welcome to the internet</h1>
 		<div class="two-boxes">
-			<img src="https://i.imgur.com/JuDcwNx.jpeg" alt=""/>
+			<img height="600px" src="https://cdn.discordapp.com/attachments/1041632587430502422/1086047786488840222/IMG_1655.JPG" alt=""/>
 			<p>sffgfdgfgfdgfdsgfsgs</p>
 		</div>
 		<h1>Twitch</h1>
