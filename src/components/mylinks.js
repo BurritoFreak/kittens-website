ad435062efec7f7b8062dcd34bf823c1137be59d
BurritoFreak <chris.sarmousakis@gmail.com>
2023-08-01 22:43:51 -0400

    removed spicy links and reordered links

--- src/components/mylinks.js
+++ src/components/mylinks.js
@@ -2,6 +2,11 @@
 //React element in json form with all of my links
 
 const myLinks = [{
+        "name": "OnlyFans",
+        "url": "https://onlyfans.com/emokitten_xoxo",
+        "icon": "fa-solid fa-pepper-hot",
+        "spicy": false
+    },{
         "name": "Discord",
         "url": "https://discord.gg/UHSSxzcH7X",
         "icon": "fab fa-discord",
@@ -12,29 +17,24 @@ const myLinks = [{
         "icon": "fab fa-instagram",
         "spicy": false
     },{
-        "name": "OnlyFans",
-        "url": "https://onlyfans.com/emokitten_xoxo",
-        "icon": "fa-solid fa-pepper-hot",
-        "spicy": true
-    },{
         "name": "Tik Tok",
         "url": "https://www.tiktok.com/@emokitten_xo",
         "icon": "fab fa-tiktok",
         "spicy": false
     },{
+        "name": "Twitch",
+        "url": "https://www.twitch.tv/emokitten_xoxo",
+        "icon": "fab fa-twitch",
+        "spicy": false
+    },{
         "name": "Reddit",
         "url": "https://www.reddit.com/user/emokitten_xoxo/",
         "icon": "fab fa-reddit",
-        "spicy": true
+        "spicy": false
     },{
         "name": "Twitter",
         "url": "https://twitter.com/emokitten_xoxo",
         "icon": "fab fa-twitter",
-        "spicy": true
-    },{
-        "name": "Twitch",
-        "url": "https://www.twitch.tv/emokitten_xoxo",
-        "icon": "fab fa-twitch",
         "spicy": false
     }];
 
--- src/pages/links.js
+++ src/pages/links.js
@@ -12,34 +12,18 @@ const spicy = [];
 var cardheight = 2;
 
 myLinks.forEach((linked, index) => {
-    if (linked.spicy) {
-        spicy.push(
-            <div key={index}>
-                <a href={linked.url} class="links"><FontAwesomeIcon icon={linked.icon} />  {linked.name}</a>
-            </div>
-            );
-    }
-    else {
-        results.push(
-            <div key={index}>
-                <a href={linked.url} class="links"><FontAwesomeIcon icon={linked.icon} />  {linked.name}</a>
-            </div>
-        );
-        };
+    results.push(
+        <div key={index}>
+            <a href={linked.url} class="links"><FontAwesomeIcon icon={linked.icon} />  {linked.name}</a>
+        </div>
+    );
 });
 
-if (spicy.length > results.length) {
-    cardheight = (spicy.length+1.5)*100;
-}
-else {
-    cardheight = (results.length+1.5)*100;
-}
+cardheight = (results.length+1.5)*100;
 
 const Links = () => {
     return ( 
     <div class="cards-list" id="cards">
-        <h1>Spicy Links</h1>
-        <div>{spicy}</div>
         <h1>Links</h1>
         <div>{results}</div>
 </div>
