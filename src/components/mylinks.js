8518ab50e5116c70febdc26419d5a3c11a4f3b80
BurritoFreak <chris.sarmousakis@gmail.com>
2024-04-11 16:19:44 -0400

    Changed links

--- src/components/mylinks.js
+++ src/components/mylinks.js
@@ -7,14 +7,9 @@ const myLinks = [{
         "icon": "fa-solid fa-pepper-hot",
         "spicy": false
     },{
-        "name": "Free OnlyFans",
-        "url": "https://onlyfans.com/emokitten_xox/c2",
-        "icon": "fa-solid fa-pepper-hot",
-        "spicy": false
-    },{
-        "name": "Discord",
-        "url": "https://discord.gg/UHSSxzcH7X",
-        "icon": "fab fa-discord",
+        "name": "Tik Tok",
+        "url": "https://www.tiktok.com/@emokitten_xoxo",
+        "icon": "fab fa-tiktok",
         "spicy": false
     },{
         "name": "Instagram",
@@ -22,24 +17,14 @@ const myLinks = [{
         "icon": "fab fa-instagram",
         "spicy": false
     },{
-        "name": "Tik Tok",
-        "url": "https://www.tiktok.com/@emokitten_xoxo",
-        "icon": "fab fa-tiktok",
-        "spicy": false
-    },{
-        "name": "Twitch",
-        "url": "https://www.twitch.tv/emokitten_xoxo",
-        "icon": "fab fa-twitch",
+        "name": "Twitter",
+        "url": "https://twitter.com/emokitten_xoxo",
+        "icon": "fab fa-twitter",
         "spicy": false
     },{
         "name": "Reddit",
         "url": "https://www.reddit.com/user/emokitten_xoxo/",
         "icon": "fab fa-reddit",
-        "spicy": false
-    },{
-        "name": "Twitter",
-        "url": "https://twitter.com/emokitten_xoxo",
-        "icon": "fab fa-twitter",
         "spicy": false
     }];
 
