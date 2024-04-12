6aa42eab4583e69333e960c6ff727f7475dc9597
BurritoFreak <chris.sarmousakis@gmail.com>
2024-04-11 20:06:28 -0400

    idk what this did

---
+++ build/static/js/627.9ed513e6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkittens_website=self.webpackChunkkittens_website||[]).push([[627],{9627:(e,t,r)=>{r.r(t),r.d(t,{default:()=>c});var n=r(5043);const c=function(e){var t=e.id;return(0,e.renderWrap)(n.createElement("iframe",{src:"https://gfycat.com/ifr/".concat(t),width:"100%",height:"500px",frameBorder:"0",scrolling:"no",allowFullScreen:!0}))}}}]);
+//# sourceMappingURL=627.9ed513e6.chunk.js.map
